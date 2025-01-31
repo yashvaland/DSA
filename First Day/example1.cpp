b616b6dca66591bfc4593a91eb9e17f8557ee9f1
valand yash <yashvaland94@gmail.com>
2025-01-31 10:15:41 +0530

    update-1

--- First Day/example1.cpp
+++ First Day/example1.cpp
@@ -1,37 +1,17 @@
 //wap to swap value of two variable wiathout using third variable and + and -;
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    
-//int x=20;
-//int y=10;   
-//
-//x=x*y;
-//y=x/y;
-//x=x/y;
-//cout<<"X="<<x<<endl;
-//cout<<"Y="<<y<<endl;
-//
-//    return 0;
-//    
-//}
-//wap to swap value of two variable using third variable;
-
-
 #include <iostream>
 using namespace std;
 
 int main() {
     
-int num1=40;
-int num2=50;
-int temp=num1;
+int x=20;
+int y=10;   
 
-num1=num2;
-num2=temp;
-
-cout<<num1<<endl<<num2;
+x=x*y;
+y=x/y;
+x=x/y;
+cout<<"X="<<x<<endl;
+cout<<"Y="<<y<<endl;
 
     return 0;
     
